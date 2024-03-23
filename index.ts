32c6cb20309f47fae6ded3bb99a63d3a5589a129
Muhammad Fayyaz <muhammad.fayyaz.edu@gmail.com>
2024-03-24 03:28:42 +0500

    changes in code

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 // await means wait for line 3 take input from user....[] this is array {message} is a object{message is a key which will appeare on screen for user,type:what will be the datatype , name is a variable to save the input}
 const answer = await inquirer.prompt([
-  { message: "ENTER FIRST NUMBER", type: "number", name: "firstNumber" },
-  { message: "ENTER SECOND NUMBER", type: "number", name: "secondNumber" },
+  { message: "ENTER your FIRST NUMBER", type: "number", name: "firstNumber" },
+  { message: "ENTER your SECOND NUMBER", type: "number", name: "secondNumber" },
   {
     message: "SELECT ANY ONE OPERATOR TO PERFOME ACTION",
     type: "list",
